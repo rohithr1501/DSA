1150af00efcd51153bd5f3d60bb204b2a8abdbfe
rohithr1501 <rohithr152126@gmail.com>
2026-07-06 06:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.12%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int left = 0;
+        int right = 0;
+
+       while(right < nums.size())
+       {
+         if(nums[right] != 0)
+         {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right++;
+         }
+         else
+         {
+            right++;
+         }
+       }
+    }
+};
